29ecd011fbb5b6e8b2793b0dd3cfff344255f17b
code-for-real <68783537+code-for-real@users.noreply.github.com>
2020-09-09 11:56:35 +0100

    refactor

--- appointments/test/Appointment.test.js
+++ appointments/test/Appointment.test.js
@@ -31,6 +31,13 @@ describe('Appointment', () => {
 describe('AppointmentDayView', () => {
     let container
 
+    
+    const today = new Date();
+    const appointments = [
+        { startsAt: today.setHours(12, 0) },
+        { startsAt: today.setHours(13, 0) }
+    ];
+
     beforeEach(() => {
         container = document.createElement('div')
     })
@@ -43,12 +50,6 @@ describe('AppointmentDayView', () => {
     })
 
     it('renders multiple appointments in an ol element', () => {
-        const today = new Date();
-        const appointments = [
-          { startsAt: today.setHours(12, 0) },
-          { startsAt: today.setHours(13, 0) }
-        ];
-
         render(<AppointmentsDayView appointments={appointments} />);
         
         expect(container.querySelector('ol')).not.toBeNull();
@@ -58,19 +59,18 @@ describe('AppointmentDayView', () => {
         ).toHaveLength(2);
       });
       
-      it('renders each appointment in an li', () => {
-        const today = new Date();
-        const appointments = [
-         { startsAt: today.setHours(12, 0) },
-         { startsAt: today.setHours(13, 0) }
-        ];
+    it('renders each appointment in an li', () => {
+
         render(<AppointmentsDayView appointments={appointments} />);
+ 
         expect(container.querySelectorAll('li')).toHaveLength(2);
+ 
         expect(
-          container.querySelectorAll('li')[0].textContent
+            container.querySelectorAll('li')[0].textContent
         ).toEqual('12:00');
+ 
         expect(
-          container.querySelectorAll('li')[1].textContent
+            container.querySelectorAll('li')[1].textContent
         ).toEqual('13:00');
-      });
+    });
 })
